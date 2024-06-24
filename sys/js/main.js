7a686b9d0eaac661ad2614127c8a47ec704928db
Shane Gadsby <Shane.Gadsby@unisq.edu.au>
2024-06-25 00:04:25 +1000

    should work and save and stuff now

--- sys/js/main.js
+++ sys/js/main.js
@@ -4,6 +4,10 @@ angular.module('Maker Skill Tree', [])
 	.controller('master', ($scope, $sce) => {
 		m = $scope
 
+		m.data = {
+			items: {}
+		}
+
 		//Trust a string as rendereable HTML
 		m.trustAsHtml = $sce.trustAsHtml
 
@@ -153,7 +157,7 @@ angular.module('Maker Skill Tree', [])
 			}
 
 			document.querySelector('.output').appendChild(svg.cloneNode(true))
-			
+
 			let svgDoc = m.convertForeignObjectsToText(document.querySelector('.output svg')),
 
 				styles = await $.get('sys/css/styles.css'),
@@ -163,8 +167,130 @@ angular.module('Maker Skill Tree', [])
 
 			styleElement.innerHTML += styles
 
-			console.log(svgDoc.outerHTML)
+			;[...svgDoc.querySelectorAll('input, foreignObject, div, p')].forEach(el => {
+				el.remove()
+			})
+
+
+			let fOs = [...document.querySelectorAll('.textbox-wrapper')],
+				newData = JSON.parse(JSON.stringify(m.data)),
+				newItems = {}
+
+			for (let i in fOs) {
+				let parentIndex = fOs[i].parentElement.getAttribute('index'),
+					_index = fOs[i].getAttribute('index')
+
+				if (!!newData.items[_index]) {
+					newItems[parentIndex] = newData.items[_index]
+				}
+			}
+
+			newData.items = newItems
+
+			let jsonElement = document.createElement('json')
+			jsonElement.innerHTML = btoa(angular.toJson(newData))
+
+			svgDoc.prepend(jsonElement)
+
+
+			let fileContent = svgDoc.outerHTML,
+				bb = new Blob([fileContent], {type: 'image/svg+xml'}),
+				a = document.createElement('a')
+
+			let title = (newData.title || 'Untitled').replace(/[^a-z0-9]/gi, '_').toLowerCase().trim()
+			a.download = `MakerSkillTree - ${title}.svg`
+			a.href = window.URL.createObjectURL(bb)
+			a.click()
+
+
+			document.querySelector('.output').replaceChildren();
 			return svgDoc.outerHTML
+		}
+
+		m.loadSVG = (event) => {
+
+			let [file] = document.querySelector("input[type=file]").files,
+				reader = new FileReader()
+
+			reader.addEventListener("load", () => {
+
+
+				document.querySelector('.output').innerHTML = reader.result
+
+				let raw = document.querySelector('.output json')
+
+				let json
+				try {
+					json = JSON.parse(atob(raw.innerHTML))
+				}
+				catch (e) {
+					console.log("Error:", e)
+				}
+
+				let oldSVG = document.querySelector('.output svg')
+				if (oldSVG) {
+					oldSVG.remove()
+				}
+
+				if (json) {
+					m.data = json
+					document.querySelector("input[type=file]").value = '',
+						fOs = [...document.querySelectorAll('.textbox-wrapper')]
+
+					for (let i in fOs) {
+						let _index = fOs[i].getAttribute('index')
+						document.querySelector(`svg:not(.output svg) foreignObject[index="${_index}"]`).appendChild(fOs[i])
+					}
+
+					m.$applyAsync()
+				}
+
+			}, false)
+
+			if (file) {
+				reader.readAsText(file);
+			}
+
+		}
+
+		m.createEditableForeignObject = (scope, $compile, ngModel, placeholder) => {
+			let fO = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject'),
+				out = $compile(`<input placeholder="${placeholder}" ng-model="${ngModel}">`)(scope)[0]
+
+
+			fO.setAttribute('x', 0)
+			fO.setAttribute('y', 0)
+			fO.setAttribute('width', 0)
+			fO.setAttribute('height', 0)
+
+
+			fO.appendChild(out)
+			return fO
+		}
+
+		m.createEditableName = (svg, scope, compile) => {
+			let fO = m.createEditableForeignObject(scope, compile, 'data.title', 'Enter a title...')
+
+			fO.setAttribute('x', 100)
+			fO.setAttribute('y', 52)
+			fO.setAttribute('width', 707)
+			fO.setAttribute('height', 24)
+
+
+			svg.appendChild(fO)
+			return fO
+		}
+
+		m.createEditableTitle = (svg, scope, compile) => {
+			let fO = m.createEditableForeignObject(scope, compile, 'data.name', 'Enter your name...')
+
+			fO.setAttribute('x', 125)
+			fO.setAttribute('y', 1114)
+			fO.setAttribute('width', 213)
+			fO.setAttribute('height', 24)
+
+			svg.appendChild(fO)
+			return fO
 		}
 
 	})
@@ -185,7 +311,6 @@ angular.module('Maker Skill Tree', [])
 				let svg = element.find('svg')[0],
 					hexagonalQuerySelector = `.cls-12:not([d="M 208.2 236.93 176.95 199.6 208.2 162.26 270.72 162.26 301.98 199.6 270.72 236.93 208.2 236.93z"]):not([d="M 104.3 278.54 73.04 241.2 104.3 203.87 166.81 203.87 198.07 241.2 166.81 278.54 104.3 278.54z"]):not([d="M 414.37 237.55 383.12 200.21 414.37 162.88 476.89 162.88 508.14 200.21 476.89 237.55 414.37 237.55z"]):not([d="M 620.54 237.8 589.28 200.46 620.54 163.13 683.05 163.13 714.31 200.46 683.05 237.8 620.54 237.8z"]):not([d="M 722.8 280.01 691.54 242.68 722.8 205.34 785.32 205.34 816.57 242.68 785.32 280.01 722.8 280.01z"])`
 
-
 				m.hexagonals = {}
 
 				//Convert the hexagonals to paths
@@ -196,10 +321,10 @@ angular.module('Maker Skill Tree', [])
 					m.convertPolyToPath(poly)
 				})
 
-
 				//Convert the hexagonals to paths
 				let paths = [...document.querySelectorAll(hexagonalQuerySelector)],
-					createTextBox = (x, y, text) => {
+					_index = 0,
+					createTextBox = (x, y, text, index) => {
 						let textbox = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject'),
 							textboxWrapper = document.createElement('div'),
 							textboxInner = document.createElement('p'),
@@ -215,26 +340,29 @@ angular.module('Maker Skill Tree', [])
 						textbox.setAttribute('y', y)
 						textbox.setAttribute('width', 148)
 						textbox.setAttribute('height', 88)
-
+						textbox.setAttribute('index', _index)
+						_index++
 
 						//buildTip(textbox)
 
 
 						textboxWrapper.classList.add('textbox-wrapper')
 						textboxWrapper.setAttribute('xmlns', 'http://www.w3.org/1999/xhtml')
+						textboxWrapper.setAttribute('index', index)
 						textboxWrapper.appendChild(textboxLeftAngles)
 						textboxWrapper.appendChild(textboxRightAngles)
 
 						textboxInner.classList.add('textbox-inner')
 						textboxInner.innerText = text
 
-						let out = $compile(textboxInner.outerHTML.replace(`></`, `>{{text}}</`), null, 1)(scope)[0]
+						let out = $compile(textboxInner.outerHTML.replace(`></`, `>{{data.items[index]}}</`), null, 1)(scope)[0]
 
 						textboxWrapper.appendChild(out)
 						textbox.appendChild(textboxWrapper)
 
 						return {textbox, textboxWrapper, textboxInner}
-					}
+					},
+					index = 0
 
 
 				paths.forEach((path) => {
@@ -242,13 +370,12 @@ angular.module('Maker Skill Tree', [])
 						{
 							textbox,
 							textboxInner
-						} = createTextBox(pos.x, pos.y, ``)
+						} = createTextBox(pos.x, pos.y, ``, index)
 
-					//console.log(text)
+					index++
 
 					svg.appendChild(textbox)
 
-					//console.log(pos)
 				})
 
 
@@ -270,18 +397,60 @@ angular.module('Maker Skill Tree', [])
 						constrainDimensions: true,
 					},
 				})
+
+				m.sortHexagonals.on('drag:stop', (a, b, c) => {
+					/*setTimeout(() => {
+
+						let fOs = [...document.querySelectorAll('.textbox-wrapper')];
+
+						let old = {}
+						for (let i in fOs) {
+							let parentIndex = fOs[i].parentElement.getAttribute('index'),
+								index = fOs[i].getAttribute('index')
+
+							if (parentIndex != index) {
+
+								old[parentIndex] = m.data.items[parentIndex]
+								
+								m.data.items[parentIndex] = m.data.items[index]
+								
+								m.data.items[index] = old[parentIndex]
+
+								fOs[i].setAttribute('index', parentIndex)
+
+								console.log(1111)
+
+							}
+							else {
+								//console.log(2222)
+							}
+						}
+
+						m.$applyAsync()
+
+					}, 250)*/
+				})
+
 				let startX, startY;
 
-
+				m.createEditableName(svg, scope, $compile)
+				m.createEditableTitle(svg, scope, $compile)
 			}
 		}
 	})
 
 	.directive('textboxInner', ($compile) => {
+		let index = 0
 		return {
 			restrict: 'C',
 			scope: true,
 			link: (scope, element, attrs) => {
+				scope.index = index
+				//element[0].//.setAttribute('index', index)
+				//element[0].parent().parent()[0].setAttribute('index', index)
+
+				index++
+
 				buildTip = async (element) => {
 					let content = $compile(await $.get(`sys/popups/edit-text.html`), null, 1)(scope)[0]
 
